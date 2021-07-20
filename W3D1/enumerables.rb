a82a2cc3ada9c9ad5f16d9677ab53a27e7accf52
Jim <jnardi1223@gmail.com>
2021-07-19 21:05:22 -0400

    first two

--- W3D1/enumerables.rb
+++ W3D1/enumerables.rb
@@ -1,17 +1,41 @@
 class Array
-
+    require 'byebug'
     def my_each(&prc)
+        i = 0 
+        #debugger 
+        while i < self.length 
+            ele = self[i]
+            prc.call(ele) 
+            i += 1 
+        end
 
-        # self.map! do |ele|
-           p prc.call(ele)
-        # end
-
-        p self
+        self
     end
+
+    # def my_each(&prc)
+    #     self.length.times do |i|
+    #       prc.call(self[i])
+    #     end
+    
+    #     self
+    #   end
 
 end
 
-arr = [1, 2, 3]
-p arr.my_each({ |ele| puts ele })
+# arr = [1, 2, 3]
+# p arr.my_each { |ele| puts ele }
 
-# do you see this?
+return_value = [1, 2, 3].my_each do |num|
+    puts num
+  end.my_each do |num|
+    puts num
+  end
+  # => 1
+       2
+       3
+       1
+       2
+       3
+  
+  p return_value  # => [1, 2, 3]
+
