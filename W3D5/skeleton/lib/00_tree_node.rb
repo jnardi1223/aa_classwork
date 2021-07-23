d9c54bf599357f0e9c6d5ec2efc658821e2987ed
Jim <jnardi1223@gmail.com>
2021-07-23 18:54:33 -0400

    dfs

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -19,19 +19,73 @@ class PolyTreeNode
         end 
     end 
 
+    def add_child(child_node)
+        child_node.parent = self 
+    end 
 
+    def remove_child(child_node)
+        raise "#{child_node} is not a child" if child_node.parent == nil 
+        child_node.parent = nil 
+    end 
+
+    def dfs(target_value)
+        return nil if self.children.empty? 
+        stack = [self]
+        return self if self.value == target_value
+
+        stack[-1].children.each do |child|
+            stack << child 
+            if stack[-1].dfs(target_value) == nil 
+                stack.pop 
+            elsif stack[-1].dfs(target_value) == target_value
+                return stack[-1].dfs(target_value) 
+            end
+            
+        end 
+
+
+
+        # self.children.each do |child| 
+        #     if !child.children.empty? 
+        #         child.dfs(target_value) 
+        #     else 
+        #         return nil  
+        #     end 
+        # end 
+
+    end 
+    
+    def bfs(target_value)
+
+    end 
 
     
 
 end
 
-a= PolyTreeNode.new("A")
-b= PolyTreeNode.new("B")
-c= PolyTreeNode.new("C")
-a.parent=(b)
-c.parent=(b)
-p b.children
-c.value= "C 2"
-c.parent=(b)
-c.parent=(b)
-p b.children
+# n1 = PolyTreeNode.new("root1")
+# n2 = PolyTreeNode.new("root2")
+# n3 = PolyTreeNode.new("root3")
+
+# # connect n3 to n1
+# n3.parent = n1
+# # connect n3 to n2
+# n3.parent = n2
+
+# # this should work
+# raise "Bad parent=!" unless n3.parent == n2
+# raise "Bad parent=!" unless n2.children == [n3]
+
+# # this probably doesn't
+# raise "Bad parent=!" unless n1.children == []
+
+# a= PolyTreeNode.new("A")
+# b= PolyTreeNode.new("B")
+# c= PolyTreeNode.new("C")
+# a.parent=(b)
+# c.parent=(b)
+# p b.children
+# c.value= "C 2"
+# c.parent=(b)
+# c.parent=(b)
+# p b.children
