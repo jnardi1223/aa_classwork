584a95f4cf16e790db731b29f275b23a553c63d4
Jim <jnardi1223@gmail.com>
2021-07-23 16:32:11 -0400

    parent=

--- W3D5/skeleton/lib/00_tree_node.rb
+++ W3D5/skeleton/lib/00_tree_node.rb
@@ -1,3 +1,21 @@
 class PolyTreeNode
 
+    attr_reader :children, :parent, :value 
+    
+
+    def initialize(value)
+        @value = value 
+        @parent = nil 
+        @children = []
+    end     
+
+    def parent=(property)
+        @parent = property
+        property.children << self 
+    end 
+
+
+
+    
+
 end
