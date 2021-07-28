f32f91cf60022c0d6fb8da96a1ac9d0ac77c993a
Jim <jnardi1223@gmail.com>
2021-07-28 19:39:20 -0400

    stepable

--- W4D1/skeleton/lib/tic_tac_toe_node.rb
+++ W4D1/skeleton/lib/tic_tac_toe_node.rb
@@ -1,11 +1,13 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  
   attr_reader :board, :next_mover_mark, :prev_move_pos
+  
   def initialize(board, next_mover_mark, prev_move_pos = nil)
     @board = board
-    @next_mover_mark= next_mover_mark
-    @prev_move_pos= prev_move_pos
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -21,15 +23,16 @@ class TicTacToeNode
   def children
     arr=[]
 
-    @board.rows.each_with_index do |row, rownum| 
-      row.each_with_index do |grid, colnum| 
-        if @board.empty?([rownum, colnum])
-          #if arr[-1].nextmovemark = :x, selfmovemark=:o
+    @board.rows.each_with_index do |row, row_num| 
+      row.each_with_index do |spot, spot_num| 
+        
+        if @board.empty?([row_num, spot_num])
           self.next_mover_mark == :x ? next_mark = :o : next_mark = :x
-          newboard= self.board.dup
-          newboard[[rownum, colnum]]= next_mover_mark
-          arr<< TicTacToeNode.new(newboard, next_mark, [rownum, colnum])
+          new_board = self.board.dup
+          new_board[[row_num, spot_num]] = next_mover_mark
+          arr << TicTacToeNode.new(new_board, next_mark, [row_num, spot_num])
         end
+
       end
     end
 
--- W4D2/piece.rb
+++ W4D2/piece.rb
@@ -2,34 +2,89 @@
 
 class Piece 
 
-    def initialize 
-        @symbol = :P
+    attr_reader :pos, :board, :color
+
+    def initialize(color, board, pos)
+       @color = color 
+       @board = board
+       @pos = pos
     end
 
-    def symbol
-        :P
+    def to_s
+        
     end
+
+    def empty?(pos) 
+
+    end
+
+    def valid_moves
+        valid = []
+        @board.each do |row|
+            row.each do |spot|
+                if spot == :E  
+                    valid << [row, spot]
+                end 
+            end 
+        end 
+        valid 
+    end 
+
+    def pos=(val)
+
+    end 
+
+    def Symbol 
+
+    end
+
+    def move_into_check?(end_pos)
+
 
 end
 
-# class Null_piece < Piece
-#     include Singleton 
+class Null_piece < Piece
+    include Singleton 
     
-#     def initialize
-#        super 
-#     end 
+    def initialize
+       super 
+    end 
 
-#     def moves 
-#         puts "..."
-#     end 
+    def moves 
+        puts "..."
+    end 
 
-#     def symbol 
-#         :n
-#     end 
+    def symbol 
+        :n
+    end 
 
-
-# end
-
-class King_Knight
 
 end
+
+class King < Piece
+
+end
+
+class Queen < Piece
+
+end
+
+class Knight < Piece
+
+end
+
+class Pawn < Piece
+
+end
+
+module Stepable
+   
+    def moves 
+    #    possible_moves = [[1, 1], [1, -1], [-1, 1] [-1, -1] [1, 0], [-1, 0], [0, 1], [0 -1]] 
+        possible_moves = [0, 1, -1]
+        b = [1, -1]
+        possible_moves.product(b) 
+
+    end
+
+end 
