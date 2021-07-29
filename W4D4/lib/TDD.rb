30468440c7dcdf1fb5729613726ee359c8b4976a
Danny Park <danincali@gmail.com>
2021-07-29 14:39:30 -0700

    stock picker

--- W4D4/lib/TDD.rb
+++ W4D4/lib/TDD.rb
@@ -1,5 +1,3 @@
-
-
 def my_uniq(arr)
     arr.uniq
 end
@@ -15,5 +13,46 @@ def two_sum(arr)
 end 
 
 
-# my_uniq([[1, 2, 1, 3, 3]])
-# two_sum([-1, 0, 2, -2, 1])
+#my_transpose
+def my_transpose(arr)
+    arr.transpose
+end
+
+# def stock_picker(daily_price)
+#     max_diff = 0
+#     i=0
+#     while i<daily_price.length
+#         curr_low = daily_price[i]
+#         curr_day = i
+#         if max_diff > curr_low
+#             # set max_diff to 1st & 2nd day's difference
+#             # check if 3rd day & 2nd day's difference > curr_low
+#         end
+#     end
+
+# end
+
+def stock_picker(daily_price)
+    # stock_price = [300,200,100,260,350,320]
+    max_diff = 0
+    max_indices = []
+    (0...daily_price.length).each do |i|
+        (i+1...daily_price.length).each do |k|
+            if (daily_price[k] - daily_price[i]) > max_diff
+                max_diff = (daily_price[k] - daily_price[i])
+                max_indices << [i,k]
+                # p "max_diff @ #{[i,k]} : #{max_diff}"
+                # p "max_indices @ #{[i,k]} : #{max_indices}"
+            end
+        end
+    end
+    # p "max_diff #{max_diff}"
+    # p "max_indices #{max_indices}"
+    return max_indices[-1]
+end
+
+
+
+# p my_uniq([1, 2, 3])
+# p two_sum([-1, 0, 2, -2, 1])
+# p stock_picker([300,200,100,260,350,320])
--- W4D4/spec/TDD_spec.rb
+++ W4D4/spec/TDD_spec.rb
@@ -1,22 +1,67 @@
 require "TDD"
 
 describe "#my_uniq" do 
-    let(:array_1) {[1,2,1,3,3]}
-    it "accepts an array" do 
-      # expect { my_uniq(array_1 + 9) }.to raise_error(TypeError)
-      my_uniq(array_1)
-    end 
+  let(:array_1) {[1,2,3]}
+  it "accepts an array" do 
+    # expect { my_uniq(array_1 + 9) }.to raise_error(TypeError)
+    my_uniq(array_1)
+  end 
+
+  # it "does not use Array#uniq" do
+  #   # my_uniq(array_1)
+  #   expect(array_1).not_to receive(:uniq)
+  # end
 
   it "returns an array of unique elements" do 
     expect(my_uniq(array_1)).to eq([1, 2, 3])
   end 
-    
+  
+
+
 end 
 
 describe "#two_sum" do  
+  let(:array_1) {[-1, 0, 2, -2, 1]}
+  it "accepts an array" do 
+    # expect { my_uniq(array_1 + 9) }.to raise_error(TypeError)
+    two_sum(array_1)
+  end 
+
   it "finds all pair indices where elements sum to 0" do 
     expect(two_sum([-1, 0, 2, -2, 1])).to eq([[0, 4], [2, 3]])
   end 
+
+
+  describe "#my_transpose" do
+    let(:array_1) {[[0, 1, 2],[3, 4, 5],[6, 7, 8]]}
+
+    it "accepts a 2-D array" do 
+      my_transpose(array_1)
+    end
+
+    it "converts between the row-oriented and column-oriented representations" do
+      expect(my_transpose(array_1)).to eq([[0, 3, 6],[1, 4, 7],[2, 5, 8]])
+    end
+
+  end
+
+
+
+  describe "#stock_picker" do 
+    let(:array_1) {[300,200,100,260,350,320]}
+
+    it "accepts an array" do 
+      stock_picker(array_1)
+    end
+
+    it "returns pair of days at which max profits occurred " do
+      expect(stock_picker(array_1)).to eq([2,4])
+    end
+
+
+  end
+  
+
 end
 
 
