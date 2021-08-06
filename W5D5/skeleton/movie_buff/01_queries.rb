910aac9bb559af920597b13f48440c38fe8317ba
Jim <jnardi1223@gmail.com>
2021-08-06 17:18:38 -0400

    Working on Harrison Ford Problem

--- W5D5/skeleton/movie_buff/01_queries.rb
+++ W5D5/skeleton/movie_buff/01_queries.rb
@@ -6,7 +6,7 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
-
+  Movie.select(:id, :title, :score).where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,7 +20,7 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
-
+  Movie.select(:id, :title).joins(:actors).where(name: 'Harrison Ford', 'castings.ord != 1')
 end
 
 def biggest_cast
