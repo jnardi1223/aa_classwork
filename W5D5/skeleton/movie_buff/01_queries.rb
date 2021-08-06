da7008161d6283e9cbbefd504a4e101c6b4a8a4c
MiDeshay <86858281+MiDeshay@users.noreply.github.com>
2021-08-06 15:23:47 -0700

    pressing on

--- W5D5/skeleton/movie_buff/01_queries.rb
+++ W5D5/skeleton/movie_buff/01_queries.rb
@@ -53,7 +53,7 @@ def directed_by_one_of(them)
   # Movie.where(yr: years)
   #
   # Find the id and title of all the movies directed by one of 'them'.
-  Movie.select(:id, :title)
+  Movie.select(:id, :title).joins(:director).where(actors: {name: them})
 end
 
 def movie_names_before_1940
@@ -67,5 +67,6 @@ def movie_names_before_1940
   # improve performace for larger queries.
   #
   # Use pluck to find the title of all movies made before 1940.
+  Movie.where('yr < 1940').pluck(:title)
 
 end
--- W5D5/skeleton/movie_buff/02_queries.rb
+++ W5D5/skeleton/movie_buff/02_queries.rb
@@ -2,12 +2,12 @@ def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
-
+  Movie.select(:id, :title, :yr, :score).where(yr: 1980..1989, score: 3..5)
 end
 
 def bad_years
   # List the years in which a movie with a rating above 8 was not released.
-
+  Movie.select(:yr).where()
 end
 
 def cast_list(title)
