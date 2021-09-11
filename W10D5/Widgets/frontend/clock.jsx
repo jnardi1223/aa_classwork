f92bcd52bbc830571ed666042d69cd40a4f4a383
Jim <jnardi1223@gmail.com>
2021-09-10 20:32:27 -0400

    got the timmer working

--- W10D5/Widgets/frontend/clock.jsx
+++ W10D5/Widgets/frontend/clock.jsx
@@ -8,23 +8,30 @@ class Clock extends React.Component {
         }
     }
 
+    tick() {
+        this.setState({date: new Date()}, () => console.log(this.state));
+        // console.log(this.state); 
+    }
+
+    componentDidMount() {
+        setInterval(this.tick.bind(this), 1000); 
+    }
+
+    
+
     render(){
         let hours = this.state.date.getHours();
         let minutes = this.state.date.getMinutes();
         let seconds = this.state.date.getSeconds();
-        console.log (this.state);
+        // console.log (this.state);
+        
         return(
             <div>
                 <h1>{hours}:{minutes}:{seconds}</h1>
             </div>
         )
     }
+    
 }
-
-
-
-
-
-
 
 export default Clock;
