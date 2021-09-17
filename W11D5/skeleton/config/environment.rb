4ecb89fbcf73ba409fbf3b4918b2876fce013561
Jim <jnardi1223@gmail.com>
2021-09-17 19:17:38 -0400

    working on first ajax request

--- W11D5/skeleton/config/environment.rb
+++ W11D5/skeleton/config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative 'application'
 
+Jbuilder.key_format camalize: :lower
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- W11D5/skeleton/frontend/pokedex.jsx
+++ W11D5/skeleton/frontend/pokedex.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import {fetchAllPokemon} from './util/api_util'
 
 document.addEventListener("DOMContentLoaded", ()=> {
+    window.fetchAllPokemon = fetchAllPokemon
+
     const root = document.getElementById("root")
     ReactDOM.render(<h1>Hello World!</h1>, root)
 })
