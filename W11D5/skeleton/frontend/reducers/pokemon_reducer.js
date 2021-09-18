06e2c7868f10b06379b3f62588d1ddb0fc58cdec
Jim <jnardi1223@gmail.com>
2021-09-17 20:01:34 -0400

    end of day Friday

--- W11D5/skeleton/frontend/reducers/pokemon_reducer.js
+++ W11D5/skeleton/frontend/reducers/pokemon_reducer.js
@@ -1,3 +1,15 @@
 import {RECEIVE_ALL_POKEMON} from '../actions/pokemon_actions'
+import { fetchAllPokemon } from '../util/api_util';
 
-export default pokemonReducer 
+
+export default pokemonReducer = (state = {}, action) => {
+    Object.freeze(state)
+    const newState = Object.assign({}, state); 
+    switch (action.type) {
+        case RECEIVE_ALL_POKEMON: 
+            action.pokemon.forEach((pokemon) => {
+                newState[entities.pokemon.id] 
+            }) 
+            // newState[]
+    }
+}
